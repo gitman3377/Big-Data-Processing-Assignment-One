e7b5e4c3f4ae02703e9ed714027a1195c1379776
gitman3377 <manmohan05012@gmail.com>
2026-08-30 16:48:47 +1000

    Update task3_join_mapper.py

--- task3_join_mapper.py
+++ task3_join_mapper.py
@@ -13,14 +13,14 @@ for line in sys.stdin:
         taxi_id = fields[0]
         company_id = fields[1]
 
-        # Composite key: taxi_id + record type 0
+        # Composite key => taxi_id + record type 0
         print(f"{taxi_id}\t0\t{company_id}")
 
-    # Trips.txt format:  trip_id, taxi_id, fare, distance,
+    # Trips.txt format =>  trip_id, taxi_id, fare, distance,
     elif len(fields) == 8:
         taxi_id = fields[1]
         fare = fields[2]
         distance = fields[3]
 
-        # Composite key:taxi_id + record type 1
+        # Composite key => taxi_id + record type 1
         print(f"{taxi_id}\t1\t{fare}\t{distance}")
